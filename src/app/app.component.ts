ba5348e0eef33c39bd1270e612bcbd8b64afaaab
Martin Jaros <martinjaros88@gmail.com>
2025-01-02 18:28:47 +0100

    Design Header section and start with the Hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { HeroSectionComponent } from './about/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroSectionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
---
+++ src/app/portfolio-showcase/portfolio-showcase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-showcase',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-showcase.component.html',
+  styleUrl: './portfolio-showcase.component.scss'
+})
+export class PortfolioShowcaseComponent {
+
+}
